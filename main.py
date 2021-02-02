65565789abf824d0464d5bcfd635139d4328c9ac
williamguilhermesouza <williamguilhermesouza@gmail.com>
2021-02-02 19:34:14 -0300

    working on extracting numeroprocesso

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import requests
 
+# getting the data from susep
 raw_data = requests.get('http://dados.susep.gov.br/olinda-ide/servico/produtos/versao/v1/odata/DadosProdutos?$format=json')
 
+# saving the raw data locally
+with open('raw_data', 'w') as output:
+    output.write(raw_data.text)
+
+# parsing raw data to dictonary format
 json_data = raw_data.json()
 
-filtered_data = [product for product in json_data if product.tipoproduto == 'PLANO DE PREVIDÊNCIA']
+# filtering the data by tipoproduto
+filtered_data = [product for product in json_data['value'] if product['tipoproduto'] == 'PLANO DE PREVIDÊNCIA']
+
+# filtering the data for process numbers
+#process_numbers = [number for number in filtered_data['numeroprocesso']]
 
 print(filtered_data)
