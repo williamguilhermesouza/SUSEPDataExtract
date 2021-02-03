1614a4bfaa9eae9cfe76580773bb3977871998e8
williamguilhermesouza <williamguilhermesouza@gmail.com>
2021-02-03 11:49:15 -0300

    added try except controls

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
-from time import sleep
 
 # getting the data from susep
-raw_data = requests.get('http://dados.susep.gov.br/olinda-ide/servico/produtos/versao/v1/odata/DadosProdutos?$format=json')
+try:
+    raw_data = requests.get('http://dados.susep.gov.br/olinda-ide/servico/produtos/versao/v1/odata/DadosProdutos?$format=json')
+except:
+    raise('Raw data query error')
 
 # saving the raw data locally
-with open('raw_data', 'w') as output:
+with open('raw_data.json', 'w') as output:
     output.write(raw_data.text)
 
 # parsing raw data to dictonary format
@@ -18,18 +20,43 @@ filtered_data = [product for product in json_data['value'] if product['tipoprodu
 # filtering the data for process numbers
 process_numbers = [number['numeroprocesso'] for number in filtered_data]
 
+with open('process_numbers.json', 'w') as output:
+    output.write(str(process_numbers))
+
+process_errors = []
+
 for number in process_numbers:
-    susep_page = requests.post('https://www2.susep.gov.br/safe/menumercado/REP2/Produto.aspx/Consultar', { 'numeroProcesso' : number })
-    
+    try:
+        susep_page = requests.post('https://www2.susep.gov.br/safe/menumercado/REP2/Produto.aspx/Consultar', { 'numeroProcesso' : number })
+    except:
+        error = f'Query error in process {number}'
+        print(error)
+        process_errors.append({number: error})
+        continue
+
     soup = BeautifulSoup(susep_page.text, 'html.parser')
 
-    pdf_link = f'https://www2.susep.gov.br{soup.a["onclick"][15:-1]}'
+    try:
+        pdf_link = f'https://www2.susep.gov.br{soup.a["onclick"][15:-1]}'
+        
+        pdf_name = f'{str(number).replace('/', '-')}.pdf'
+    except:
+        error = f'PDF Link construction error in process {number}'
+        print(error)
+        process_errors.append({number: error})
+        continue
 
-    pdf_name = f'{pdf_link[-5:]}.pdf'
 
-    pdf = requests.get(pdf_link)
+    try:
+        pdf = requests.get(pdf_link)
+    except:
+        error = f'PDF error in process {number}'
+        print(error)
+        process_errors.append({number: error})
+        continue
 
     with open(pdf_name, 'wb') as output:
         output.write(pdf.content)
 
-
+with open('errors', 'w') as output:
+    output.write(str(process_errors))
