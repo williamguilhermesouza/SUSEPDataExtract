e9a307ba763358360af0eeda03b6d990dbc641ef
williamguilhermesouza <williamguilhermesouza@gmail.com>
2021-02-08 20:41:19 -0300

    adding json dump and commenting main

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import queue
 import threading
+import json
 from PdfProcessing import PdfDownloader
 
 # getting the data from susep
@@ -23,9 +24,11 @@ filtered_data = [product for product in json_data['value'] if product['tipoprodu
 # filtering the data for process numbers
 process_numbers = [number['numeroprocesso'] for number in filtered_data]
 
+# dumping process numbers to a json
 with open('process_numbers.json', 'w') as output:
-    output.write(str(process_numbers))
+    output.write(json.dumps(process_numbers))
 
+# creating lists of errors, output, queue and threads
 process_errors = []
 outputJSON = []
 numbers_queue = queue.Queue(-1)
@@ -34,55 +37,37 @@ queueLock = threading.Lock()
 exit_flag = 0
 
 
+# putting the process numbers to queue
 queueLock.acquire()
 for number in process_numbers:
     numbers_queue.put(number)
 queueLock.release()
 
-
+# outputting feedback of total process numbers
 print(f'Process total: {numbers_queue.qsize()}')
 
+# creating threads using pdf downloader class
 for i in range(10):
     threads.append(PdfDownloader(numbers_queue, exit_flag, process_errors, queueLock, outputJSON))
     threads[i].start()
 
+# holding main thread until queue is empty
 while not numbers_queue.empty():
     pass
 
+# when queue is empty flagging to threads that they can exit
 exit_flag = 1
 
+# joining all threads so they can exit
 for t in threads:
     t.join()
 
+# outputting results to json
 with open('output.json', 'w') as output:
-    output.write(str(outputJSON))
+    output.write(json.dumps(outputJSON))
 
+# outputting errors to json
 with open('errors.json', 'w') as output:
-    output.write(str(process_errors))
-
-# retying errors
-
-error_numbers = [number['numeroprocesso'] for number in process_errors]
-threads = []
-exit_flag = 0
+    output.write(json.dumps(process_errors))
 
 
-queueLock.acquire()
-for number in error_numbers:
-    numbers_queue.put(number)
-queueLock.release()
-
-
-print(f'Errors total: {numbers_queue.qsize()}')
-
-for i in range(10):
-    threads.append(PdfDownloader(numbers_queue, exit_flag, process_errors, queueLock, outputJSON))
-    threads[i].start()
-
-while not numbers_queue.empty():
-    pass
-
-exit_flag = 1
-
-for t in threads:
-    t.join()
